7096f72296295c9e923ed83cc4d4f0191ef94523
sujanayata <sujanayata@gmail.com>
2026-01-05 22:11:52 +0530

    commit

---
+++ practice/practice/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React  from "react";
+import Home from "./components/Home";
+import About from "./components/about";
+import Project from "./components/project";
+import Contact from "./components/contact";
+const App = () => {
+  return (
+    <div>
+     <Home/>
+     <About/>
+     <Project/>
+     <Contact/>
+    </div>
+  );
+};
+export default App;
