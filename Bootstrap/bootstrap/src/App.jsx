960b4a5989ee704a9c3e397426c3cf19c418a62e
sujanayata <sujanayata@gmail.com>
2026-01-27 16:24:58 +0530

    bootstrap

---
+++ Bootstrap/bootstrap/src/App.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import { Bootstrap } from "./component/Bootstrap"
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Bootstrap></Bootstrap>
+       
+    </>
+  )
+}
+
+export default App
---
+++ Bootstrap/bootstrap/src/component/Bootstrap.jsx
@@ -0,0 +1,14 @@
+export function Bootstrap(){
+    return(
+        <>
+        <button className="btn btn-primary">click here</button>
+        <p><a class="link-opacity-10-hover" href="#">Link hover opacity 10</a></p>
+        <p class="text-black bg-white">.text-black</p>
+        <p class="text-white bg-dark">.text-white</p>
+        <p class="text-black-50 bg-white">.text-black-50</p>
+        <p class="text-white-50 bg-dark">.text-white-50</p>
+
+
+        </>
+    )
+}
