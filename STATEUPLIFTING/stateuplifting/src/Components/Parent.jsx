28ed38bfa79d6ba80139327fb187b4145cf6c304
sujanayata <sujanayata@gmail.com>
2026-02-06 13:24:21 +0530

    state uplifting

--- STATEUPLIFTING/stateuplifting/src/Components/Parent.jsx
+++ STATEUPLIFTING/stateuplifting/src/Components/Parent.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 
-export function Parent(){
+export function Parent() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <ChildA count={count} />
+      <ChildB setCount={setCount} />
+      <ChildC count={count}></ChildC>
+    </>
+  );
+}
+
+function ChildA({ count }) {
+  return <h1>Count: {count}</h1>;
+}
+
+function ChildB({ setCount }) {
+  return (
+    <button onClick={() => setCount(prev => prev + 1)}>
+      Increment Count
+    </button>
+  );
+}
+function ChildC({count}){
     return(
         <>
-          
-
+         <h1> count : {count}</h1>
         </>
     )
 }
